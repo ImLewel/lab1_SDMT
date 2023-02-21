9fb0a78fcb62f46d6e96cd3df9ee7298a4a6054f
Lev Pavelko <neroheroes6@gmail.com>
2023-02-21 22:57:23 +0200

    Added equation solution logic

--- QuadraticEqSolver/QuadraticEqSolver/Program.cs
+++ QuadraticEqSolver/QuadraticEqSolver/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿float a, b, c, D, x1, x2;
+a = float.Parse(Console.ReadLine());
+b = float.Parse(Console.ReadLine());
+c = float.Parse(Console.ReadLine());
+
+D = b * b - 4 * a * c;
+
+if (D > 0) {
+  x1 = (-b - MathF.Sqrt(D)) / (2 * a);
+  x2 = (-b + MathF.Sqrt(D)) / (2 * a);
+  Console.WriteLine($"There are two roots \n" +
+    $"x1: {x1} \n" +
+    $"x2: {x2}");
+}
+if (D < 0) Console.WriteLine("There are 0 roots");
+if (D == 0) {
+  x1 = (-b + MathF.Sqrt(D)) / (2 * a);
+  Console.WriteLine($"There is 1 root \n" +
+    $"x: {x1}");
+}
