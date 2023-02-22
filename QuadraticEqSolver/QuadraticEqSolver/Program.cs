a0b42d95a166f99e6e7bc458b0b07a0c7533386f
Lev Pavelko <neroheroes6@gmail.com>
2023-02-22 15:48:36 +0200

    Added not interactive mode

--- QuadraticEqSolver/QuadraticEqSolver/Program.cs
+++ QuadraticEqSolver/QuadraticEqSolver/Program.cs
@@ -1,14 +1,27 @@
-﻿namespace QuadraticEqSolver {
+﻿
+
+namespace QuadraticEqSolver {
   class Program {
     static void Main(string[] args) {
-
       float[] koefs = new float[3];
       float temp = 0;
       float D, x1, x2;
+      bool interactiveMode;
+      string[] fileData = new string[3];
+
+      if (args.Length != 0 && File.Exists(args[0])) {
+        interactiveMode = false;
+        fileData = File.ReadAllText(args[0]).Split(" ");
+      }
+      else
+        interactiveMode = true;
+
 
       void Setup() {
         for (int i = 0; i < koefs.Length; ++i) {
-          while (ValueTester(i) == false) { }
+          if (interactiveMode)
+            while (ValueTester(i) == false) { }
+          else NInterValueTester(i, fileData);
           koefs[i] = temp;
         }
       }
@@ -32,6 +45,26 @@
           return false;
         }
       }
+
+      bool NInterValueTester(int i, string[] toTest) {
+        if (toTest[i] == "" || toTest[i] == null) {
+          Console.WriteLine("Error. Empty koef");
+          return false;
+        }
+        else if (float.TryParse(toTest[i], out temp)) {
+          if (i == 0 && temp == 0) {
+            Console.WriteLine("a can't be zero");
+            return false;
+          }
+          else
+            return true;
+        }
+        else {
+          Console.WriteLine($"Error. Expected a valid real number, got {toTest[i]} instead");
+          return false;
+        }
+      }
+
       float FindDiscriminant(float a, float b, float c) => (b * b) - (4 * a * c);
 
       void Solve(float a, float b, float c) {
@@ -51,6 +84,7 @@
 
       Setup();
       Solve(koefs[0], koefs[1], koefs[2]);
+      Console.ReadLine();
     }
   }
 }
